03a8c1ce6be0224ec0f776b7662cd98b12d2a2b3
gerolvr <gerolvr@gmx.com>
2017-10-30 09:09:09 +1100

    Commit initial starter projects

---
+++ ConstellationClientService/src/main/java/com/gerolivo/constellationgenerator/ConstellationClientServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gerolivo.constellationgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstellationClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstellationClientServiceApplication.class, args);
+	}
+}
---
+++ StarsService/src/main/java/com/gerolivo/constellationgenerator/StarsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gerolivo.constellationgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarsServiceApplication.class, args);
+	}
+}
