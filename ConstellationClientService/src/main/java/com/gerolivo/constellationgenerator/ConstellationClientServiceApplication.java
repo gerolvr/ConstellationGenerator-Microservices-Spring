85dbef29fcb70319d2e5b145fa475d6a9ad1dbe3
gerolvr <gerolvr@gmx.com>
2017-10-30 13:28:17 +1100

    Add Sleuth sampler to follow traces in Zipkin

--- ConstellationClientService/src/main/java/com/gerolivo/constellationgenerator/ConstellationClientServiceApplication.java
+++ ConstellationClientService/src/main/java/com/gerolivo/constellationgenerator/ConstellationClientServiceApplication.java
@@ -2,10 +2,23 @@ package com.gerolivo.constellationgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
 public class ConstellationClientServiceApplication {
 
+	@Bean
+	public AlwaysSampler defaultSampler() {
+	  return new AlwaysSampler();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConstellationClientServiceApplication.class, args);
 	}
